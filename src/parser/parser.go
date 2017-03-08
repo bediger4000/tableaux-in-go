3987cf855d2a024058a413096f4db7be311e4409
Bruce Ediger <bediger@stratigery.com>
2017-03-07 21:53:40 -0700

    Parse a PL expression

---
+++ src/parser/parser.go
@@ -0,0 +1,41 @@
+package parser
+
+import (
+	"lexer"
+	"node"
+	"fmt"
+)
+
+type Parser struct {
+	lexer *lexer.Lexer
+}
+
+func New(lexer *lexer.Lexer) (*Parser) {
+	var parser Parser
+	parser.lexer = lexer
+	return &parser
+}
+
+func (p *Parser) Parse() (n *node.Node) {
+
+	token, typ := p.lexer.NextToken()
+
+	fmt.Printf("%q type %s\n", token, lexer.TokenName(typ))
+
+	switch typ {
+	case lexer.NOT:
+		n = node.NewOpNode(lexer.NOT)
+		n.Left = p.Parse()
+	case lexer.LPAREN:
+		left := p.Parse()
+		n = p.Parse()
+		n.Left  = left
+		n.Right = p.Parse()
+		_, _ = p.lexer.NextToken()  // should be LPAREN
+	case lexer.AND, lexer.OR, lexer.IMPLIES, lexer.EQUIV:
+		n = node.NewOpNode(typ)
+	case lexer.IDENT:
+		n = node.NewIdentNode(token)
+	}
+	return n
+}
