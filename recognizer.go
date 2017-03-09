812edc312521c705866bc00a0b31c6740063a94e
Bruce Ediger <bediger@stratigery.com>
2017-03-09 16:48:35 -0700

    Copy of current parsetest.go, which just recognizes prop logic expressions

---
+++ recognizer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"lexer"
+	"fmt"
+	// "node"
+	"os"
+	"parser"
+)
+
+func main() {
+	var lxr *lexer.Lexer
+	if len(os.Args) > 1 {
+		lxr = lexer.NewFromFileName(os.Args[1])
+	} else {
+		lxr = lexer.NewFromFile(os.Stdin)
+	}
+
+	psr := parser.New(lxr)
+
+	r := psr.Recognizer()
+
+	if r {
+		fmt.Printf("It's an expression\n")
+	} else {
+		fmt.Printf("It's NOT an expression\n")
+	}
+}
