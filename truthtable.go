529bfc19b0fbf8e63db570682ee6dea2fba93b0d
Bruce Ediger <bediger@stratigery.com>
2017-03-10 15:28:40 -0700

    Add printing of actual truth table and headers.

--- truthtable.go
+++ truthtable.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"lexer"
+	"log"
 	"node"
 	"os"
 	"parser"
@@ -31,10 +32,46 @@ func printTruthTable(root *node.Node) {
 
 	identifiers := findIdentifiers(root)
 
-	fmt.Printf("Found %d identifiers: %v\n", len(identifiers), identifiers)
+	max := len(identifiers)
+	var vals []bool
+	for i := 0; i < max; i++ {
+		vals = append(vals, true)
+	}
+
+	for _, variable := range identifiers {
+		n := 5 - len(variable)
+		spacer := ""
+		for i := 0; i < n; i++ {
+			spacer += " "
+		}
+		fmt.Printf("%s%s ", spacer, variable)
+	}
+	expression := node.ExpressionToString(root)
+	fmt.Printf("\t%s\n", expression)
+
+	for {
+
+		valuation := make(map[string]bool)
+		for idx, id := range identifiers {
+			valuation[id] = vals[idx]
+		}
+		r := evaluateExpression(root, valuation)
+		printRow(identifiers, vals, r)
+
+		var idx int
+		for idx = max - 1; idx >= 0; idx-- {
+			vals[idx] = !vals[idx]
+			if !vals[idx] {
+				break
+			}
+		}
+		if idx < 0 {
+			break
+		}
+	}
 }
 
-func findIdentifiers(n *node.Node) ([]string) {
+func findIdentifiers(n *node.Node) []string {
 
 	allIdentifiers := findAllIdentifiers(n)
 
@@ -52,7 +89,7 @@ func findIdentifiers(n *node.Node) ([]string) {
 	return uniqIdentifiers
 }
 
-func findAllIdentifiers(n *node.Node) ([]string) {
+func findAllIdentifiers(n *node.Node) []string {
 
 	var identifiers []string
 	if n.Op == lexer.IDENT {
@@ -67,4 +104,39 @@ func findAllIdentifiers(n *node.Node) ([]string) {
 		identifiers = append(identifiers, ids...)
 	}
 	return identifiers
+}
+
+func evaluateExpression(n *node.Node, valuation map[string]bool) bool {
+	switch n.Op {
+	case lexer.NOT:
+		return !evaluateExpression(n.Left, valuation)
+	case lexer.AND:
+		return evaluateExpression(n.Left, valuation) && evaluateExpression(n.Right, valuation)
+	case lexer.OR:
+		return evaluateExpression(n.Left, valuation) || evaluateExpression(n.Right, valuation)
+	case lexer.IMPLIES:
+		p := evaluateExpression(n.Left, valuation)
+		q := evaluateExpression(n.Right, valuation)
+		if !p && q {
+			return false
+		}
+		return true
+	case lexer.EQUIV:
+		return evaluateExpression(n.Left, valuation) == evaluateExpression(n.Right, valuation)
+	case lexer.IDENT:
+		return valuation[n.Ident]
+	}
+	log.Fatalf("Problem with node type %s (%d): shouldn't get here\n", lexer.TokenName(n.Op), n.Op)
+	return false
+}
+
+func printRow(identifiers []string, vals []bool, r bool) {
+	for idx, _ := range identifiers {
+		spacer := " "
+		if !vals[idx] {
+			spacer = ""
+		}
+		fmt.Printf("%s%v ", spacer, vals[idx])
+	}
+	fmt.Printf("\t%v\n", r)
 }
