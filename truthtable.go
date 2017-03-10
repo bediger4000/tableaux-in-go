1ea67159043adaf0c396aa7abb80205951b91cde
Bruce Ediger <bediger@stratigery.com>
2017-03-10 11:17:38 -0700

    Now it finds a list of all identifers in the expression

--- truthtable.go
+++ truthtable.go
@@ -6,6 +6,7 @@ import (
 	"node"
 	"os"
 	"parser"
+	"sort"
 )
 
 func main() {
@@ -21,8 +22,49 @@ func main() {
 	var root *node.Node
 	root = psr.Parse()
 
-	if root != nil {
-		root.Print(os.Stdout)
-		fmt.Printf("\n")
+	printTruthTable(root)
+
+	os.Exit(0)
+}
+
+func printTruthTable(root *node.Node) {
+
+	identifiers := findIdentifiers(root)
+
+	fmt.Printf("Found %d identifiers: %v\n", len(identifiers), identifiers)
+}
+
+func findIdentifiers(n *node.Node) ([]string) {
+
+	allIdentifiers := findAllIdentifiers(n)
+
+	alreadySeen := make(map[string]bool)
+	var uniqIdentifiers []string
+	for _, id := range allIdentifiers {
+		if _, ok := alreadySeen[id]; !ok {
+			alreadySeen[id] = true
+			uniqIdentifiers = append(uniqIdentifiers, id)
+		}
 	}
+
+	sort.Strings(uniqIdentifiers)
+
+	return uniqIdentifiers
+}
+
+func findAllIdentifiers(n *node.Node) ([]string) {
+
+	var identifiers []string
+	if n.Op == lexer.IDENT {
+		identifiers = append(identifiers, n.Ident)
+	}
+	if n.Left != nil {
+		ids := findAllIdentifiers(n.Left)
+		identifiers = append(identifiers, ids...)
+	}
+	if n.Right != nil {
+		ids := findAllIdentifiers(n.Right)
+		identifiers = append(identifiers, ids...)
+	}
+	return identifiers
 }
