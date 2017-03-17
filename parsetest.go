fc3c0c2dc0cd6aff569d21e056f76e92fc0f107d
Bruce Ediger <bediger@stratigery.com>
2017-03-17 16:50:39 -0600

    Add graphviz output, io.Writer (bytes.Buffer) off cmd line

--- parsetest.go
+++ parsetest.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"lexer"
 	"node"
@@ -9,9 +11,15 @@ import (
 )
 
 func main() {
+
+	graphVizOutputFilename := flag.String("g", "", "File name for graphviz output, no default")
+	flag.Parse()
+
 	var lxr *lexer.Lexer
-	if len(os.Args) > 1 {
-		lxr = lexer.NewFromFileName(os.Args[1])
+	if flag.NArg() > 0 {
+		expressions := flag.Args()
+		expr := bytes.NewBufferString(expressions[0] + "\n")
+		lxr = lexer.NewFromFile(expr)
 	} else {
 		lxr = lexer.NewFromFile(os.Stdin)
 	}
@@ -24,5 +32,15 @@ func main() {
 	if root != nil {
 		root.Print(os.Stdout)
 		fmt.Printf("\n")
+	}
+
+	if *graphVizOutputFilename != "" {
+		fout, err := os.OpenFile(*graphVizOutputFilename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Problem opening %q write-only: %s\n", *graphVizOutputFilename, err)
+			os.Exit(1)
+		}
+		defer fout.Close()
+		root.GraphNode(fout)
 	}
 }
